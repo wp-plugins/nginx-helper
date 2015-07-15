5333790aff6246872117d5026f6c7d96e8f1eb0b
rtcamp <rtcamp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 14:33:51 +0000

    Tagging version 1.9.4
    git-svn-id: https://plugins.svn.wordpress.org/nginx-helper/tags/1.9.4@1199390 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nginx-helper.php
+++ nginx-helper.php
@@ -2,8 +2,8 @@
 /*
   Plugin Name: Nginx Helper
   Plugin URI: https://rtcamp.com/nginx-helper/
-  Description: Cleans nginx's fastcgi/proxy cache or redis-cahce whenever a post is edited/published. Also does few more things.
-  Version: 1.9.3
+  Description: Cleans nginx's fastcgi/proxy cache or redis-cache whenever a post is edited/published. Also does few more things.
+  Version: 1.9.4
   Author: rtCamp
   Author URI: https://rtcamp.com
   Text Domain: nginx-helper
